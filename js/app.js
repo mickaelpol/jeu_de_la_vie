8fc7dc3111fe488fdb3c40ef44616033abbccf19
mickael pol <polmickael3@gmail.com>
2017-09-12 17:32:08 +0200

    test de la boucle id
    essaie de creer l'id unique sur chacune des cases

--- js/app.js
+++ js/app.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
 	var line = 10;
 	var col = 10;
+ var idUnique;
 
-	for (var i = 0; i < line; i++) {
-		if (i%2 === 0) {
-			$('#table').append('<tr class="line"></tr>');
-		} else {
-			$('#table').append('<tr class="underline"></tr>');
-		}
-	}
+ for (var i = 1; i <= 100; i++) {
+   idUnique = i;
+   console.log(idUnique);
+ }
 
-	for (var i = 0; i < col; i++) {
-		if (i%2 === 0) {
-			$('.line').append('<td></td>');
-			$('.underline').append('<td class="border"></td>');
-		} else {
-			$('.underline').append('<td></td>');
-			$('.line').append('<td class="border"></td>');
-		}
-	}
 
+
+ for (var i = 0; i < line; i++) {
+  if (i%2 === 0) {
+   $('#table').append('<tr class="line"></tr>');
+ } else {
+   $('#table').append('<tr class="underline"></tr>');
+ }
+}
+
+for (var i = 0; i < col; i++) {
+
+  if (i%2 === 0) {
+   $('.line').append('<td id="'+ idUnique+'"></td>');
+   $('.underline').append('<td id="" class="border"></td>');
+ } else {
+   $('.underline').append('<td id=""></td>');
+   $('.line').append('<td id="" class="border"></td>');
+ }
+}
 });
