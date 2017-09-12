73add3240e570037805d8741646de8b19dbeb8aa
mickael pol <polmickael3@gmail.com>
2017-09-12 16:31:36 +0200

    génération Js
    génération du tableau en Js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
+	var line = 10;
+	var col = 10;
 
+	for (var i = 0; i < line; i++) {
+		if (i%2 === 0) {
+			$('#table').append('<tr class="line"></tr>');
+		} else {
+			$('#table').append('<tr class="underline"></tr>');
+		}
+	}
 
+	for (var i = 0; i < col; i++) {
+		if (i%2 === 0) {
+			$('.line').append('<td></td>');
+			$('.underline').append('<td class="border"></td>');
+		} else {
+			$('.underline').append('<td></td>');
+			$('.line').append('<td class="border"></td>');
+		}
+	}
 
 });
